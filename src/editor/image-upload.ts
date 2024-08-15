e9d841c4dc5597180e9d32e64fe421856d5ab86c
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-08-15 17:58:01 +0300

    Connect image upload to cloudinary api

--- src/editor/image-upload.ts
+++ src/editor/image-upload.ts
@@ -1,48 +1,41 @@
 import { createImageUpload } from "novel/plugins";
 
 const onUpload = (file: File) => {
-  // const promise = fetch("/api/upload", {
-  //   method: "POST",
-  //   headers: {
-  //     "content-type": file?.type || "application/octet-stream",
-  //     "x-vercel-filename": file?.name || "image.png",
-  //   },
-  //   body: file,
-  // });
+  const cloudName = "dio8jfh7f";
+  const unsignedUploadPreset = "conversion_hive_unsigned";
 
-  // return new Promise((resolve) => {
-  //   promise.then(async (res) => {
-  //     // Successfully uploaded image
-  //     if (res.status === 200) {
-  //       const { url } = await res.json();
-  //       // preload the image
-  //       const image = new Image();
-  //       image.src = url;
-  //       image.onload = () => {
-  //         resolve(url);
-  //       };
-  //       // No blob store configured
-  //     } else if (res.status === 401) {
-  //       resolve(file);
-  //       throw new Error(
-  //         "`BLOB_READ_WRITE_TOKEN` environment variable not found, reading image locally instead."
-  //       );
-  //       // Unknown error
-  //     } else {
-  //       throw new Error(`Error uploading image. Please try again.`);
-  //     }
-  //   });
-  // });
+  const fd = new FormData();
+  fd.append("upload_preset", unsignedUploadPreset);
+  fd.append("tags", "browser_upload"); // Optional - add tags for image admin in Cloudinary
+  fd.append("file", file);
+
+  const promise = fetch(`https://api.cloudinary.com/v1_1/${cloudName}/upload`, {
+    method: "POST",
+    body: fd,
+  });
 
   return new Promise((resolve) => {
-    const image = new Image();
-    image.src =
-      "https://images.unsplash.com/photo-1721332153282-3be1f363074d?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-    image.onload = () => {
-      resolve(
-        "https://images.unsplash.com/photo-1721332153282-3be1f363074d?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      );
-    };
+    promise.then(async (res) => {
+      // Successfully uploaded image
+      if (res.status === 200) {
+        const { url: secure_url } = await res.json();
+        // preload the image
+        const image = new Image();
+        image.src = secure_url;
+        image.onload = () => {
+          resolve(secure_url);
+        };
+        // No blob store configured
+      } else if (res.status === 401) {
+        resolve(file);
+        throw new Error(
+          "`BLOB_READ_WRITE_TOKEN` environment variable not found, reading image locally instead."
+        );
+        // Unknown error
+      } else {
+        throw new Error(`Error uploading image. Please try again.`);
+      }
+    });
   });
 };
 
